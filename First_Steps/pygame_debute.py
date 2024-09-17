fe9d1909090185666aba6cd139b63d295bdcf326
Maz0k <114288731+Maz0ky@users.noreply.github.com>
2024-09-17 16:21:12 +0200

    Snail rect

--- First_Steps/pygame_debute.py
+++ First_Steps/pygame_debute.py
@@ -1,27 +1,30 @@
 import pygame
 from sys import exit
 
+path_for_files = "Visi301_Mathieu_Teva/First_Steps"
+
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Two Steps Ahead")#titrew
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('First_Steps/font/Pixeltype.ttf',50)
+test_font = pygame.font.Font(path_for_files + '/font/Pixeltype.ttf',50)
+
 
 """
-test_surface = pygame.Surface((100,200))
-test_surface.fill('Red')
+test_surf = pygame.surf((100,200))
+test_surf.fill('Red')
 """
 
 
-sky_surface = pygame.image.load('First_Steps/graphics/Sky.png').convert()
-ground_surface = pygame.image.load('First_Steps/graphics/ground.png').convert()
-text_surface = test_font.render('Big GG', False, 'Purple')
+sky_surf = pygame.image.load(path_for_files + '/graphics/Sky.png').convert()
+ground_surf = pygame.image.load(path_for_files + '/graphics/ground.png').convert()
+text_surf = test_font.render('Big GG', False, 'Purple')
 
-snail_surface = pygame.image.load('First_Steps/graphics/snail/snail1.png').convert_alpha() #convert = optimise les images
-snail_x_pos = 600
+snail_surf = pygame.image.load(path_for_files + '/graphics/snail/snail1.png').convert_alpha() #convert = optimise les images
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
-player_surface = pygame.image.load('First_Steps/graphics/Player/player_walk_1.png').convert_alpha()
-player_rect = player_surface.get_rect(midbottom = (80,300))
+player_surf = pygame.image.load(path_for_files + '/graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300)) #assigne un rect à player un sprite où le midbottom sera aux co 80 300
 
 while True:
     for event in pygame.event.get():
@@ -29,13 +32,17 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(sky_surface,(0,0))#pose une surface sur l'ecran du jeu
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface, (350,50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100 :    snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos,250))
-    screen.blit(player_surface, player_rect)
+    screen.blit(sky_surf,(0,0))#pose une surface sur l'ecran du jeu
+    screen.blit(ground_surf,(0,300))
+    screen.blit(text_surf, (350,50))
+    
+    # snail_x_pos -= 4
+    snail_rect.x -= 4
+    if snail_rect.right < 0 :    snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+    
+    player_rect.left += 1
+    screen.blit(player_surf, player_rect)
             
-    pygame.display.flip()
+    pygame.display.flip() #update tout l'ecran
     clock.tick(60) # max 60fps pour le jeu
