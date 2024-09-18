61e2226b397cb2d330afea90471a26ce27abaaf4
Maz0k <114288731+Maz0ky@users.noreply.github.com>
2024-09-18 22:16:28 +0200

    collisions

--- First_Steps/pygame_debute.py
+++ First_Steps/pygame_debute.py
@@ -18,7 +18,8 @@ test_surf.fill('Red')
 
 sky_surf = pygame.image.load(path_for_files + '/graphics/Sky.png').convert()
 ground_surf = pygame.image.load(path_for_files + '/graphics/ground.png').convert()
-text_surf = test_font.render('Big GG', False, 'Purple')
+
+text_surf = test_font.render('Big GG', False, 'Purple')#(text, antialias ,color) The antialias argument is a boolean: if True the characters will have smooth edges
 
 snail_surf = pygame.image.load(path_for_files + '/graphics/snail/snail1.png').convert_alpha() #convert = optimise les images
 snail_rect = snail_surf.get_rect(bottomright = (600, 300))
@@ -31,6 +32,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('collision')
+        
+    """
+    if event.type == pygame.MOUSEMOTION:
+        event.pos # donne les co (x,y) position de la souris
+    MOUSEBUTTONUP / DOWN 
+    """
     
     screen.blit(sky_surf,(0,0))#pose une surface sur l'ecran du jeu
     screen.blit(ground_surf,(0,300))
@@ -41,8 +51,19 @@ while True:
     if snail_rect.right < 0 :    snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
     
-    player_rect.left += 1
+    # player_rect.left += 1
     screen.blit(player_surf, player_rect)
             
+    # if player_rect.colliderect(snail_rect):#envoie un booléen qui dit s'il y a collision entre les deux rect
+    #     print('collision')
+        
+    """
+    rect.collidepoint((x,y)) check si le point est dans le rect
+    """
+    mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())# envoie une liste de bool des clique(droit,millieu,droit)
+        
+    
     pygame.display.flip() #update tout l'ecran
     clock.tick(60) # max 60fps pour le jeu
