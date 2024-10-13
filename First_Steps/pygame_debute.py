3ff39fdb67068f0ca3029c5a5342a72f9f431ecf
Mathieu Brunot <brunotmathieu340@gmail.com>
2024-10-13 23:12:42 +0200

    begining

--- First_Steps/pygame_debute.py
+++ First_Steps/pygame_debute.py
@@ -19,22 +19,29 @@ test_surf.fill('Red')
 sky_surf = pygame.image.load(path_for_files + '/graphics/Sky.png').convert()
 ground_surf = pygame.image.load(path_for_files + '/graphics/ground.png').convert()
 
-text_surf = test_font.render('Big GG', False, 'Purple')#(text, antialias ,color) The antialias argument is a boolean: if True the characters will have smooth edges
+score_surf = test_font.render('Big GG', False, (64,64,64))#(text, antialias ,color) The antialias argument is a boolean: if True the characters will have smooth edges
+score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load(path_for_files + '/graphics/snail/snail1.png').convert_alpha() #convert = optimise les images
 snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
 player_surf = pygame.image.load(path_for_files + '/graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300)) #assigne un rect à player un sprite où le midbottom sera aux co 80 300
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos):
-        #         print('collision')
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
         
     """
     if event.type == pygame.MOUSEMOTION:
@@ -44,15 +51,30 @@ while True:
     
     screen.blit(sky_surf,(0,0))#pose une surface sur l'ecran du jeu
     screen.blit(ground_surf,(0,300))
-    screen.blit(text_surf, (350,50))
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect,20)
+    #pygame.draw.line(screen, "Gold", pygame.mouse.get_pos(),(800,400),10)
+    pygame.draw.ellipse(screen,"Brown",pygame.Rect(50,200,100,100))#draw circle
+    screen.blit(score_surf, score_rect)
     
     # snail_x_pos -= 4
     snail_rect.x -= 4
     if snail_rect.right < 0 :    snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
     
+    #---PLAYER---
     # player_rect.left += 1
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
     screen.blit(player_surf, player_rect)
+    
+    
+    
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print('jump')
             
     # if player_rect.colliderect(snail_rect):#envoie un booléen qui dit s'il y a collision entre les deux rect
     #     print('collision')
