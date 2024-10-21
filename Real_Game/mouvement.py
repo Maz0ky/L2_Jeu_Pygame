79e7307a726b114d2cff335d251d1047491418b1
Mathieu Brunot <brunotmathieu340@gmail.com>
2024-10-21 22:43:05 +0200

    application mouvement à partir d'une liste v1

--- Real_Game/mouvement.py
+++ Real_Game/mouvement.py
@@ -1,6 +1,38 @@
-#[{mouvement:temps},{}]
-ex_tab_mouv = []
 
-def traite_mouv(mouv,tps):
-    return None
+
+class Player:
+    def __init__(self,coo,hp) -> None:
+        self.coo = coo
+        self.hp = hp
+        self.surf = pygame.image.load(path_for_files + '/graphics/Player/player_walk_1.png').convert_alpha()
+        self.rect = self.surf.get_rect(bottomleft = self.coo )
+        
+    def kill(self):
+        self.hp -= 1
+        
+    def move_of(self, vect):
+        self.rect.move(vect)
+        
+        
+class File:
+    
+    def __init__(self,file=[]):
+        """ Instancie une file vide """
+        self.file = file #un tableau
+		
+    def enfiler(self, element):
+        """ Enfile un élément en queue de file """
+        self.file.append(element)
+		
+    def defiler(self):
+        """ Défile ( si la file n'est pas vide ! ) un élément en tête de file, et le renvoie """
+        if not self.est_vide():
+            return self.file.pop(0)
+        
+    def est_vide(self):
+        """ Renvoie True si la file est vide, False sinon """
+        return len(self.file) == 0
+    
+    def affiche(self):
+        return self.file
 
--- Real_Game/run.py
+++ Real_Game/run.py
@@ -1,10 +1,36 @@
 import pygame
+from mouvement import *
 from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Zeta Jeu de la muerta")
 clock = pygame.time.Clock()
+path_for_files = "Visi301_Mathieu_Teva/First_Steps"
+speed = 30
+
+sky_surf = pygame.image.load(path_for_files + '/graphics/Sky.png').convert()
+ground_surf = pygame.image.load(path_for_files + '/graphics/ground.png').convert()
+
+player_surf = pygame.image.load(path_for_files + '/graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300)) #assigne un rect à player un sprite où le midbottom sera aux co 80 300
+player_gravity = 0
+
+#[{"mouvement":str,"temps":float},{}]
+ex_tab_mouv = File([{"mouvement":"r","temps":4}, {"mouvement":"r","temps":4},
+{"mouvement":"j","temps":4}])
+
+def traite_mouv(File:File,rect):
+    global player_gravity
+    mouvement = File.defiler()
+    mouv = mouvement["mouvement"]
+    if mouv == "r":#droite
+        rect.right += speed
+    elif mouv == "l":#gauche
+        rect.right -= speed
+    elif mouv == "j":#jump
+        player_gravity = -15
+            
 
 list_moving = []
 while True:
@@ -12,7 +38,29 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -15
     
+    screen.blit(sky_surf,(0,0))#pose une surface sur l'ecran du jeu
+    screen.blit(ground_surf,(0,300))
+    
+    player_gravity += 0.5
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+    
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_RIGHT] or keys[pygame.K_d]: 
+    #     player_rect.right += speed
+    # if keys[pygame.K_LEFT] or keys[pygame.K_q]:
+    #     player_rect.right -= speed
+    if not ex_tab_mouv.est_vide():
+        traite_mouv(ex_tab_mouv, player_rect)
+    
+    
+    screen.blit(player_surf, player_rect)
         
     pygame.display.flip() #update tout l'ecran
     clock.tick(60) # max 60fps pour le jeu
