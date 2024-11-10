cfd1e01d2f7f9e42087e6bb4c78901ec684b5f53
TevaPhilippe05 <117692440+TevaPhilippe05@users.noreply.github.com>
2024-11-10 22:51:13 +0000

    Fix de certains bugs + réorganisation des fichiers

--- Real_Game/mouvement.py
+++ Real_Game/mouvement.py
@@ -12,8 +12,7 @@ class Player:
         
     def move_of(self, vect):
         self.rect.move(vect)
-        
-        
+            
 class File_mouv:
     
     def __init__(self,file=[]):
@@ -50,3 +49,66 @@ class File_mouv:
         if not self.est_vide():
             return self.file[0]
 
+# Mouvement du joueur
+
+def traite_mouv(File: File_mouv, rect, speed, player_gravity):
+    mouv = File.get_mouv()["mouvement"]
+    if File.est_ecoule():
+        File.defiler_mouv()
+        mouv = File.get_mouv()
+    File.defiler_temps()
+    player_gravity = bouge(mouv, rect, speed, player_gravity)
+    return player_gravity
+
+def bouge(mouv, rect, speed, player_gravity):
+    if mouv == "r":  # Droite
+        rect.right += speed
+    elif mouv == "l":  # Gauche
+        rect.right -= speed
+    elif mouv == "j":  # Saut
+        player_gravity = -5
+    return player_gravity
+
+# Gestion de la gravité
+
+def gestion_gravitee(player_rect, player_gravity):
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+        player_gravity = 0
+    else:
+        player_gravity += 0.5
+    return player_rect, player_gravity
+
+# Generation de la liste des éléments déplaçables
+
+def generer_liste_elements(elements_deplacables):
+    """Trie et génère la liste d'éléments déplaçables"""
+    elements_triee = sorted(elements_deplacables, key=lambda elem: elem[1].x)
+    liste_elements = []
+    for elem in elements_triee:
+        if elem[2] == "elem/up-arrow.png":
+            mouv = "j"
+        elif elem[2] == "elem/right-arrow.png":
+            mouv = "r"
+        elif elem[2] == "elem/left-arrow.png":
+            mouv = "l"
+        
+        liste_elements.append({"mouvement": mouv, "temps": elem[3]})
+    return liste_elements
+
+def traiter_envoie(genere_liste_elements, elements_deplacables, player_gravity, click_again, ex_tab_mouv, player_rect, speed):
+    """Gère l'envoi d'une liste d'éléments"""
+    if not ex_tab_mouv.est_vide():
+        player_gravity = traite_mouv(ex_tab_mouv, player_rect, speed, player_gravity)
+    else:
+        click_again = True #Réactive envoie
+    
+    if genere_liste_elements:
+        liste_mouvements = generer_liste_elements(elements_deplacables)  # Retourne la liste des mouvements pour l'utiliser dans Code 3
+        print("Mouvements générés :", liste_mouvements)
+        for mouvement in liste_mouvements:
+            ex_tab_mouv.enfiler_mouv(mouvement)  # Ajoute à la file des mouvements
+        genere_liste_elements = False
+
+    return genere_liste_elements, player_gravity, click_again
--- Real_Game/run.py
+++ Real_Game/run.py
@@ -1,102 +1,33 @@
+# Import
 import pygame
+from Initialisations import *
 from mouvement import *
-from liste_element import *
+from gestion_page import *
 from sys import exit
 
+# Gestion de la fenêtre
 pygame.init()
 screen = pygame.display.set_mode((1600, 400))  # Nouvelle taille de fenêtre
 pygame.display.set_caption("Zeta Jeu de la muerta")
-
 clock = pygame.time.Clock()
 fps = 60
-speed = 10
 
-sky_surf = pygame.image.load('graphics/Sky.png').convert()
-ground_surf = pygame.image.load('graphics/ground.png').convert()
-
-player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(midbottom=(880, 300))  # Position initiale du joueur
-player_gravity = 0
-
-# Initialisation de la file des mouvements
-ex_tab_mouv = File_mouv([])
-# ex_tab_mouv = File_mouv([{"mouvement": "r", "temps": 30}, {"mouvement": "l", "temps": 3},
-#                         {"mouvement": "j", "temps": 4}, {"mouvement": "l", "temps": 23}])
-
-def bouge(mouv, rect):
-    global player_gravity
-    if mouv == "r":  # Droite
-        rect.right += speed
-    elif mouv == "l":  # Gauche
-        rect.right -= speed
-    elif mouv == "j":  # Saut
-        player_gravity = -15
-
-def traite_mouv(File: File_mouv, rect):
-    mouv = File.get_mouv()["mouvement"]
-    if File.est_ecoule():
-        File.defiler_mouv()
-        mouv = File.get_mouv()
-    File.defiler_temps()
-    bouge(mouv, rect)
-
-
-### [liste_element]
-# Variables de configuration initiale (générales pour une exécution persistante)
-
-elements_fixes, elements_deplacables, selected_element, mouse_offset = initialiser_interface() # Initialisation de divers élements
-button_font, button_text, button_rect = initialiser_bouton_envoi()  # Initialisation de l'interface avec les éléments
-menu_visible, menu_rect, option_supprimer, option_temps, element_concerne = initialiser_menu() # Initialisation du menu (suppression et temps)
-### [liste_element]
+# Initialisation
+elements_fixes, elements_deplacables, selected_element, mouse_offset, click_again, button_font, button_text, button_rect, menu_visible, menu_rect, option_supprimer, option_temps, element_concerne, ex_tab_mouv, player_surf, player_rect, player_gravity, speed = init()
 
 # Game loop principal
 while True:
+    # Gestion évenements
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                player_gravity = -15
-
-        ### [liste_element]
-        elements_deplacables, mouse_offset, genere_liste_elements, selected_element, menu_visible, menu_rect, option_supprimer, option_temps, element_concerne = eventss(screen, event, elements_fixes, elements_deplacables, selected_element, mouse_offset, button_rect, menu_visible, menu_rect, option_supprimer, option_temps, element_concerne)
-        ### [liste_element]
-
-    screen.blit(sky_surf, (0, 0))  # Affichage de l'arrière-plan
-    screen.blit(ground_surf, (0, 300))  # Affichage du sol
+        elements_deplacables, mouse_offset, genere_liste_elements, selected_element, menu_visible, menu_rect, option_supprimer, option_temps, element_concerne, player_rect, click_again = gestion_evenements(screen, event, elements_fixes, elements_deplacables, selected_element, mouse_offset, button_rect, menu_visible, menu_rect, option_supprimer, option_temps, element_concerne, click_again, player_rect, player_surf)
     
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
-        player_gravity = 0
-    else:
-        player_gravity += 0.5
-    
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-    #     player_rect.right += speed
-    # if keys[pygame.K_LEFT] or keys[pygame.K_q]:
-    #     player_rect.right -= speed
-    
-    # Si des mouvements sont dans la file, on les traite
-    if not ex_tab_mouv.est_vide():
-        
-        traite_mouv(ex_tab_mouv, player_rect)
+    # Gestion de la gravitée
+    player_rect, player_gravity = gestion_gravitee(player_rect, player_gravity)
 
-    ### [liste_element]
-    # Récupérer et appliquer les mouvements générés par l'interface
-    selected_element, button_rect, menu_visible, menu_rect, option_supprimer, option_temps = interface_ajout_mouvements(screen, elements_fixes, elements_deplacables, selected_element, mouse_offset, button_text, button_rect, menu_visible, menu_rect, option_supprimer, option_temps)
-    if genere_liste_elements:  # Si des mouvements sont générés
-        liste_mouvements = generer_liste_elements(elements_deplacables)  # Retourne la liste des mouvements pour l'utiliser dans Code 3
-        print("Mouvements générés :", liste_mouvements)
-        for mouvement in liste_mouvements:
-            ex_tab_mouv.enfiler_mouv(mouvement)  # Ajoute à la file des mouvements
-    ### [liste_element]
+    # Traite l'envoie d'une liste d'éléments
+    genere_liste_elements, player_gravity, click_again = traiter_envoie(genere_liste_elements, elements_deplacables, player_gravity, click_again, ex_tab_mouv, player_rect, speed)
 
-    # Afficher le joueur
-    screen.blit(player_surf, player_rect)
+    # Mise à jour de la page
+    button_rect, menu_visible, menu_rect, option_supprimer, option_temps = mise_a_jour_page(screen, elements_fixes, elements_deplacables, button_text, button_rect, menu_visible, menu_rect, option_supprimer, option_temps, player_surf, player_rect, clock, fps)
     
-    pygame.display.flip()  # Met à jour l'écran
-    clock.tick(fps)  # Limite à 60 FPS
+    
