07882ef67387b1d8bd1f3c54f97dc18f42f93d91
PHILIPPE Teva <philippe.teva.pro@gmail.com>
2024-11-21 10:12:20 +0100

    Réorganisation des fichiers

---
+++ Real_Game/Tuile/Format_ptx/player_sprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="player_sprite" tilewidth="24" tileheight="24" tilecount="48" columns="8">
+ <image source="../Image/player_sprite.png" width="192" height="144"/>
+</tileset>
