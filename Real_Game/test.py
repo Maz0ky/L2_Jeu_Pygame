13026e4f15cab31ef5ad36115559a2157565266d
Mathieu Brunot <brunotmathieu340@gmail.com>
2024-11-25 22:48:16 +0100

    ajout de block qui tuent

--- Real_Game/test.py
+++ Real_Game/test.py
@@ -32,11 +32,11 @@ class Fatal_Block(Tile):
 class Player(pygame.sprite.Sprite):
     def __init__(self, start_pos):
         super().__init__()
-        self.image = pygame.image.load("Real_Game/Tuile/Image/Sprite_Player_72x72/Shrek-1.png")
+        self.image = pygame.image.load("Real_Game/Tuile/Image/Player_Sprite/Shrek-1.png")
         self.rect = self.image.get_rect()
         self.start_pos = start_pos
         self.rect.bottomleft = start_pos
-        
+        self.game_over = False
         self.on_ground = False
 
         self.vitesse = pygame.math.Vector2(0,0)
@@ -47,10 +47,13 @@ class Player(pygame.sprite.Sprite):
     def get_rect(self):
         return self.rect
         
-    def update(self,b_grp):
+    def update(self,b_grp,fatal_grp):
         self.collisionx(b_grp)
         self.collisiony(b_grp)
         self.applique_vitesse(b_grp)
+        self.touch_hurting_block(fatal_grp)
+        if self.game_over:
+            print('bruh')
            
     def applique_vitesse(self,b_grp):
         self.rect.x  += self.vitesse.x
@@ -59,9 +62,15 @@ class Player(pygame.sprite.Sprite):
         self.vitesse.x = 0
         self.gravity()
     
-    def check_on_ground(self,b_grp):
+    def check_on_ground(self,b_grp): 
         collision = self.get_hit(b_grp)
-        if not(collision):
+        cheat_fly = True
+        i = 0
+        while cheat_fly and i < len(collision):
+            if self.rect.collidepoint(collision[i].rect.midtop):
+                cheat_fly = False
+            i += 1
+        if cheat_fly:
             self.on_ground = False
         
     def jump(self):
@@ -94,6 +103,12 @@ class Player(pygame.sprite.Sprite):
             elif (self.vitesse.y < 0 and self.rect.collidepoint(block.rect.midbottom)):#si touche un block du haut
                 self.vitesse.y = 0
                 self.rect.y = block.rect.bottom
+    
+    def touch_hurting_block(self,sprite_grp):
+        if self.get_hit(sprite_grp):
+            self.game_over = True
+            
+    ########## MOUVEMENTS ##########
         
     def move(self,sens:str):
         assert sens in ('j','l','r','p'), "Doit appartenir à un mouvement connu"
@@ -104,9 +119,9 @@ class Player(pygame.sprite.Sprite):
                 self.vitesse.x = -4
             case 'j':
                 self.jump()
-       
-    #ne pas oublier d'importer la classe File_mouv dans le dossier                 
+                        
     def move_from_File(self, File):
+        #ne pas oublier d'importer la classe FileMouv
         sens = File.get_mouv()["mouvement"]
         if File.est_ecoule():#si le temps du premier mouvement est terminé passe au suivant
             File.defiler_mouv()
@@ -115,8 +130,16 @@ class Player(pygame.sprite.Sprite):
         self.move(sens)
         return File
     
+    ########## MORT ET RESTART ##########
+    
     def respawn(self):
+        self.game_over = False
         self.rect.bottomleft = self.start_pos
+        
+    def is_dead(self):
+        return self.game_over
+    
+    
 
 def add_list_int(list1,list2):
     assert len(list1) == len(list2), "les deux tableaux ne s'additionnent pas"
@@ -133,6 +156,7 @@ size_tileset = 32
 map = load_pygame('Real_Game/map/map_test.tmx')
 sprite_group = pygame.sprite.Group()#groupe regroupant toutes les tuiles de la map
 block_group = pygame.sprite.Group()
+fatal_group = pygame.sprite.Group()
 
 def creer_tuile(tuiles, attribut:str=''):
     for x,y,surf in tuiles:#creer une tuile
@@ -142,7 +166,8 @@ def creer_tuile(tuiles, attribut:str=''):
                     block = Solid_Block(pos = pos, surf = surf, groups = sprite_group)
                     block_group.add(block)
                 case 'fatal':
-                    Fatal_Block(pos = pos, surf = surf, groups = sprite_group)
+                    fatal = Fatal_Block(pos = pos, surf = surf, groups = sprite_group)
+                    fatal_group.add(fatal)
                 case _:
                     Tile(pos = pos, surf = surf, groups = sprite_group)
 
@@ -150,7 +175,7 @@ def creer_tuile(tuiles, attribut:str=''):
 for layer in map.visible_layers:
     if layer.name == 'Block':
         creer_tuile(layer.tiles(),'block')
-    elif layer.name == 'Deathful':
+    elif layer.name == 'Fatal':
         creer_tuile(layer.tiles(),'fatal')
     elif hasattr(layer,'data'):#si la couche a des données alors
         creer_tuile(layer.tiles())
@@ -167,7 +192,7 @@ while True:
         
     sprite_group.draw(screen)
     Joueur.show(screen)
-    Joueur.update(block_group)
+    Joueur.update(block_group,fatal_group)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
         Joueur.move('r')
